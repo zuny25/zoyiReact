8c8c3b52d84580c923811794483d43cd49bfb89e
KimHyongJun <>
2018-06-29 16:28:54 +0900

    implement search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,15 +1,93 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  border: 1px solid #eee;
+  height: 20px;
+  padding: 0 5px;
+  margin: 0;
+`;
+const InputWrapper = styled.div`
+  margin-bottom: 10px;
+`;
+const SearchBtn = styled.button`
+  padding: 3px;
+  height: 22px;
+`;
+const Description = styled.div`
+  color: #666;
+  font-size: 12px;
+  padding: 10px 10px 0 10px;
+  font-weight: bold;
+`;
+const DescSpan = styled.span`
+  display: inline-block;
+  margin-right: 5px;
+  font-weight: normal;
+`;
+
 
 export default class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    const initialState = {
+      query: props.query,
+    };
+
+    this.state = initialState;
+    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.searchCountry = this.searchCountry.bind(this);
+  }
+
+  handleChange(event) {
+    const query = event.target.value;
+    this.setState({ query });
+  }
+
+  handleKeyDown(event) {
+    if (event.keyCode === 13) {
+      this.searchCountry();
+    }
+  }
+
+  searchCountry() {
+    const { query } = this.state;
+    const { onSearch } = this.props;
+    onSearch(query);
+  }
+
   render() {
     const {
-      onSearch,
-    } = this.props;
+      query,
+    } = this.state;
+
+    const oldQuery = this.props.query;
 
     return (
-      <div onClick={() => onSearch('b')}>
-        Search
-      </div>
+      <InputWrapper>
+        <Input
+          type="text"
+          value={query}
+          onChange={this.handleChange}
+          onKeyDown={this.handleKeyDown}
+        />
+        <SearchBtn onClick={this.searchCountry}>
+          Search
+        </SearchBtn>
+        {
+          oldQuery !== ''
+            ? (
+              <Description>
+                <DescSpan>
+                  Searching :
+                </DescSpan>
+                {`${oldQuery}`}
+              </Description>
+            )
+            : null
+        }
+      </InputWrapper>
     );
   }
 }
